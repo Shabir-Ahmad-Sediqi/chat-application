f54f7f5cdc31689d48c5f5556a55779a3c40d6d0
shabir <shabirsediqi124@gmail.com>
2025-11-12 21:40:10 +0430

    Prapared the project for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,28 @@ import express, { Request, Response } from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import sendMessage from "./routes/message.route.js"
+import path from "path"
 
-const app = express()
 dotenv.config()
+
+const app = express();
+const __dirname = path.resolve();
+
 //Middlewares
 app.use(express.json())
 
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", sendMessage)
+
+// Making the app ready for deployment.
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res: Response) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 // A simple route for testing
 app.get("/", (req: Request, res: Response) => {
@@ -20,5 +33,5 @@ app.get("/", (req: Request, res: Response) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server is litsening on port localhost:${PORT}`)
+    console.log(`Server is litsening on port http://localhost:${PORT}/`)
 })
