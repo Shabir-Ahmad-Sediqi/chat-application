c5e87ab2636b56ab4c16ea12e714756b7b66af8a
Shabir Ahmad Sediqi <shabirsediqi124@gmail.com>
2025-12-09 10:44:50 +0430

    Fixed Unbounded memory issue

--- backend/src/middleware/multer.ts
+++ backend/src/middleware/multer.ts
@@ -1,2 +1,13 @@
 import multer from "multer";
-export const upload = multer({ storage: multer.memoryStorage() });
+export const upload = multer(
+    {
+    storage: multer.memoryStorage(),
+    limits: {fileSize: 2 * 1024 * 1024},
+    fileFilter: (req: any, file: any, cb: any) => {
+        if (!file.mimetype.startsWith("image/")){
+            return cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE", "Only images allowed"));
+        }
+        cb(null, true)
+    }, 
+
+     });
